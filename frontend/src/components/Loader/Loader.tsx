b572d8c137b0152bc6f8386206c21e0ffd81857e
Fabien Charlier <fabien.charlier@student-cs.fr>
2023-05-03 15:42:52 +0200

    working loader

---
+++ frontend/src/components/Loader/Loader.tsx
@@ -0,0 +1,3 @@
+import LoaderLogo from "./loader.svg"
+
+export const Loader = () => <img src={LoaderLogo} alt="Chargement de la page en cours" />
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import styles from "./Home.module.css"
 import { Pokemon } from "components/Pokemon"
+import { Loader } from "components/Loader"
 
 export const Home = () => {
   interface PokemonInfo {
@@ -11,6 +12,7 @@ export const Home = () => {
   }
 
   const [pokemonsList, setPokemonsList] = React.useState<PokemonInfo[]>([])
+  const [isLoading, setIsLoading] = React.useState<boolean>(true)
 
   const fetchPokemons = () => {
     return fetch("http://localhost:8000/pokemons", { headers: { accept: "application/json" } })
@@ -21,6 +23,7 @@ export const Home = () => {
       const response = await fetchPokemons()
       const pokemonData = await response.json()
       setPokemonsList(pokemonData)
+      setIsLoading(false)
     }
     loadPokemons()
   }, [])
@@ -28,11 +31,17 @@ export const Home = () => {
   return (
     <div className={styles.intro}>
       <div className={styles.title}>Pokedex !</div>
-      <div className={styles.pokemonList}>
-        {pokemonsList.map(({ name, id, weight, height }) => {
-          return <Pokemon name={name} id={id} weight={weight} height={height} key={id} />
-        })}
-      </div>
+      {!isLoading ? (
+        <div className={styles.pokemonList}>
+          {pokemonsList.map(({ name, id, weight, height }) => {
+            return <Pokemon name={name} id={id} weight={weight} height={height} key={id} />
+          })}
+        </div>
+      ) : (
+        <div className={styles.loader}>
+          <Loader />
+        </div>
+      )}
     </div>
   )
 }
