b00424bba14a6587db696fb736475cbfe80fba39
Fabien Charlier <fabien.charlier@student-cs.fr>
2023-05-03 14:49:52 +0200

    design over

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -1,10 +1,14 @@
-export const Pokemon = ({ name, id }: Props) => {
+import styles from "./Pokemon.module.css"
+
+export const Pokemon = ({ name, id, weight, height }: Props) => {
   const imageUrl: string = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id + ".png"
   return (
-    <div>
+    <div className={styles.pokemonFrame}>
+      <p className={styles.pokemonName}>{name}</p>
       <img src={imageUrl} alt="Pokémon {name}" />
-      <p>Nom : {name}</p>
       <p>Id : {id}</p>
+      <p>Poids : {weight} kg</p>
+      <p>Taille : {height} cm</p>
     </div>
   )
 }
@@ -12,4 +16,6 @@ export const Pokemon = ({ name, id }: Props) => {
 interface Props {
   name: string
   id: number
+  weight: number
+  height: number
 }
