1aeefb61c78ccb593ba5f8ad1adbabf1bed59434
Fabien Charlier <fabien.charlier@student-cs.fr>
2023-05-03 10:53:06 +0200

    adding generic pokemons and list structure

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,18 +1,29 @@
 import styles from "./Home.module.css"
+import { Pokemon } from "components/Pokemon"
+
+const pokemonsList = [
+  {
+    name: "Carapuce",
+    id: 7,
+  },
+  {
+    name: "Carabaffe",
+    id: 8,
+  },
+  {
+    name: "Tortank",
+    id: 9,
+  },
+]
 
 export const Home = () => {
   return (
     <div className={styles.intro}>
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
-      <div>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
-          alt="Pokémon Carapuce, d'id 7"
-        />
-        <p>Name : Carapuce</p>
-        <p>Id : 7</p>
-      </div>
+      {pokemonsList.map(({ name, id }) => {
+        return <Pokemon name={name} id={id} key={id} />
+      })}
     </div>
   )
 }
