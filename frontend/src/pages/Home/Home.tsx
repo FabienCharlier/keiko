0fd2b1fa668e747521a844e794d30fafdb851c9a
Fabien Charlier <fabien.charlier@student-cs.fr>
2023-05-03 09:59:14 +0200

    My first Pokemon

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -5,6 +5,14 @@ export const Home = () => {
     <div className={styles.intro}>
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
+      <div>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
+          alt="Pokémon Carapuce, d'id 7"
+        />
+        <p>Name : Carapuce</p>
+        <p>Id : 7</p>
+      </div>
     </div>
   )
 }
